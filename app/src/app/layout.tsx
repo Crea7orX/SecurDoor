bba15f0316afa740b15bd97f36cc32e0c3ad1e77
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2024-10-13 11:34:18 +0300

    feat(app): Add dotted background

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "@/styles/globals.css";
-import {ClerkProvider} from "@clerk/nextjs";
-
+import { ClerkProvider } from "@clerk/nextjs";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import * as React from "react";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -14,10 +14,13 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-      <ClerkProvider>
-        <html lang="en" className={`${GeistSans.variable}`}>
-        <body>{children}</body>
-        </html>
-      </ClerkProvider>
+    <ClerkProvider>
+      <html lang="en" className={`${GeistSans.variable}`}>
+        <body className="relative min-h-screen bg-background">
+          <div className="absolute -z-50 h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]" />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
