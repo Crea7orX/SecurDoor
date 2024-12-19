645377cb3c7f46716f71707096fb8b514d38152a
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2024-12-19 21:33:02 +0200

    feat(app): Create devices db schema.ts

---
+++ app/src/server/db/devices/schema.ts
@@ -0,0 +1,16 @@
+import { sql } from "drizzle-orm";
+import { integer, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const devices = pgTable("devices", {
+  id: varchar("id", { length: 256 }).primaryKey(),
+  name: varchar("name", { length: 256 }).notNull(),
+  serialId: uuid("serial_id").notNull(),
+  key: varchar("key", { length: 6 }).notNull(),
+  ownerId: varchar("owner_id", { length: 256 }).notNull(),
+  createdAt: integer("created_at")
+    .default(sql`(EXTRACT(EPOCH FROM NOW()))`)
+    .notNull(),
+  updatedAt: integer("updated_at").$onUpdate(
+    () => sql`(EXTRACT(EPOCH FROM NOW()))`,
+  ),
+});
