e39b274dda0e3191decc918c10e387fa8cef1e7c
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2024-12-19 21:30:12 +0200

    feat(app): Create ids.ts utils

---
+++ app/src/lib/ids.ts
@@ -0,0 +1,13 @@
+enum IdPrefix {
+  DEVICE = "dvc_",
+}
+
+export default IdPrefix;
+
+export function generateId(prefix: IdPrefix, length = 32) {
+  let randomId = "";
+  for (let i = 0; i < length; i++) {
+    randomId += Math.floor(Math.random() * 10).toString();
+  }
+  return `${prefix}${randomId}`;
+}
