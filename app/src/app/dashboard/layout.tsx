3f85897833c9acec1d403b31ea5f7712bad1c0e4
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2024-12-21 18:47:59 +0200

    style(app): Update dashboard layout.tsx div

--- app/src/app/dashboard/layout.tsx
+++ app/src/app/dashboard/layout.tsx
@@ -13,9 +13,7 @@ export default function DashboardLayout({
       <DashboardSidebar />
       <SidebarInset className="bg-transparent">
         <Header />
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          {children}
-        </div>
+        {children}
       </SidebarInset>
     </SidebarProvider>
   );
