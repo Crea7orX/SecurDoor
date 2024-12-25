85630479d80f19790bfa9fccfaf12f29d2cfeaa1
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2024-12-25 18:52:16 +0200

    feat(app): Use tailwind indicator component in root layout.tsx

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { ApiProvider } from "@/components/providers/api-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { TailwindIndicator } from "@/components/ui/tailwind-indicator";
 import { ClerkProvider } from "@clerk/nextjs";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
@@ -23,6 +24,7 @@ export default function RootLayout({
             <div className="absolute -z-50 h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]" />
             {children}
             <Toaster />
+            <TailwindIndicator />
           </body>
         </html>
       </ApiProvider>
