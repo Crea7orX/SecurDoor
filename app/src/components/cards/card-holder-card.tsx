f760222b2b9736adbc5edc50863e93f5cfb6e334
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-01-12 18:10:25 +0200

    feat(app): Implement renaming holder of card in card-holder-card.tsx component

--- app/src/components/cards/card-holder-card.tsx
+++ app/src/components/cards/card-holder-card.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -5,11 +7,25 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
 import { InputInline } from "@/components/ui/input-inline";
-import { cn } from "@/lib/utils";
-import { CardResponse } from "@/lib/validations/card";
+import { useUpdateCardMutation } from "@/hooks/api/cards/use-update-card-mutation";
+import {
+  CardResponse,
+  CardUpdate,
+  cardUpdateSchema,
+} from "@/lib/validations/card";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { User } from "lucide-react";
 import * as React from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
 interface CardHolderCardProps extends React.HTMLAttributes<HTMLDivElement> {
   card: CardResponse;
@@ -17,6 +33,56 @@ interface CardHolderCardProps extends React.HTMLAttributes<HTMLDivElement> {
 
 const CardHolderCard = React.forwardRef<HTMLDivElement, CardHolderCardProps>(
   ({ className, card, ...props }, ref) => {
+    const { mutateAsync: update } = useUpdateCardMutation({
+      id: card.id,
+    });
+    const [isLoading, setIsLoading] = React.useState(false);
+
+    const form = useForm<CardUpdate>({
+      resolver: zodResolver(cardUpdateSchema),
+      defaultValues: {
+        holder: card.holder ?? "",
+      },
+    });
+
+    React.useEffect(() => {
+      if (!card) return;
+      form.setValue("holder", card.holder ?? "", {
+        shouldValidate: true,
+      });
+    }, [form, card]);
+
+    const resetForm = () => {
+      form.setValue("holder", card.holder ?? "", {
+        shouldValidate: true,
+      });
+    };
+
+    const handleUpdate = async (value: string) => {
+      if (value === card.holder) return;
+      if (!form.formState.isValid) return resetForm();
+
+      setIsLoading(true);
+      const toastId = toast.loading("Renaming card...");
+      await update({
+        holder: value,
+      })
+        .then(() => {
+          toast.success("Card renamed!", {
+            id: toastId,
+          });
+        })
+        .catch(() => {
+          resetForm(); // Reset to original value
+
+          toast.error("Failed to rename card!", {
+            id: toastId,
+          });
+        });
+
+      setIsLoading(false);
+    };
+
     return (
       <Card className={className} ref={ref} {...props}>
         <CardHeader>
@@ -27,14 +93,29 @@ const CardHolderCard = React.forwardRef<HTMLDivElement, CardHolderCardProps>(
           <CardDescription>Holder of the card</CardDescription>
         </CardHeader>
         <CardContent className="flex flex-col gap-2">
-          <InputInline
-            className={cn(
-              "bg-secondary text-secondary-foreground",
-              !card.holder && "text-muted-foreground",
-            )}
-            value={card.holder ?? ""}
-            placeholder={!card.holder ? "Unknown" : "Holder"}
-          />
+          <Form {...form}>
+            <form>
+              <FormField
+                control={form.control}
+                name="holder"
+                render={({ field }) => (
+                  <FormItem className="flex items-center gap-3">
+                    <FormControl>
+                      <InputInline
+                        className="bg-secondary text-secondary-foreground"
+                        placeholder={!card.holder ? "Unknown" : "Holder"}
+                        disabled={isLoading}
+                        onUpdate={handleUpdate}
+                        {...field}
+                        value={field.value ?? ""}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
         </CardContent>
       </Card>
     );
