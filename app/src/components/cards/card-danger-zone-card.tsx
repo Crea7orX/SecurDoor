b388f5a06923fcf3ec6fbe5643ffdc759a7b6eef
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-01-12 18:11:23 +0200

    feat(app): Implement delete card in card-danger-zone-card.tsx component

--- app/src/components/cards/card-danger-zone-card.tsx
+++ app/src/components/cards/card-danger-zone-card.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -6,8 +17,11 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { useDeleteCardMutation } from "@/hooks/api/cards/use-delete-card-mutation";
 import { Trash, TriangleAlert } from "lucide-react";
+import { useRouter } from "next/navigation";
 import * as React from "react";
+import { toast } from "sonner";
 
 interface CardDangerZoneCardProps extends React.HTMLAttributes<HTMLDivElement> {
   id: string;
@@ -17,26 +31,76 @@ const CardDangerZoneCard = React.forwardRef<
   HTMLDivElement,
   CardDangerZoneCardProps
 >(({ className, id, ...props }, ref) => {
-  // todo: fetch data from api with id
+  const router = useRouter();
+
+  const { mutateAsync: doDelete } = useDeleteCardMutation({
+    id: id,
+  });
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const handleDelete = async () => {
+    setIsLoading(true);
+    const toastId = toast.loading("Deleting card...");
+    await doDelete()
+      .then(() => {
+        toast.warning("Card deleted successfully!", {
+          id: toastId,
+        });
+
+        setIsOpen(false);
+        router.push("/dashboard/cards");
+      })
+      .catch(() => {
+        toast.error("Failed to delete card!", {
+          id: toastId,
+        });
+      });
+
+    setIsLoading(false);
+  };
 
   return (
-    <Card className={className} ref={ref} {...props}>
-      <CardHeader>
-        <CardTitle className="inline-flex items-center gap-1 text-destructive">
-          <TriangleAlert className="size-6" />
-          <span>Danger Zone</span>
-        </CardTitle>
-        <CardDescription>
-          Dangerous actions that can not be undone
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="flex items-stretch gap-2 max-md:flex-col">
-        <Button variant="destructive">
-          <Trash />
-          <span>Remove</span>
-        </Button>
-      </CardContent>
-    </Card>
+    <>
+      <Card className={className} ref={ref} {...props}>
+        <CardHeader>
+          <CardTitle className="inline-flex items-center gap-1 text-destructive">
+            <TriangleAlert className="size-6" />
+            <span>Danger Zone</span>
+          </CardTitle>
+          <CardDescription>
+            Dangerous actions that can not be undone
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex items-stretch gap-2 max-md:flex-col">
+          <Button
+            variant="destructive"
+            disabled={isLoading}
+            onClick={() => setIsOpen(true)}
+          >
+            <Trash />
+            <span>Remove</span>
+          </Button>
+        </CardContent>
+      </Card>
+      <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete this
+              card and remove its access to all doors.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel disabled={isLoading}>Cancel</AlertDialogCancel>
+            <Button disabled={isLoading} onClick={() => handleDelete()}>
+              Continue
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 });
 CardDangerZoneCard.displayName = "CardDangerZoneCard";
