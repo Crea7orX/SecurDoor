911a0ee986de097bcac0252b1187487f7178a8cc
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-01-12 18:09:57 +0200

    feat(app): Create use-delete-card-mutation.ts hook

---
+++ app/src/hooks/api/cards/use-delete-card-mutation.ts
@@ -0,0 +1,29 @@
+import { axiosInstance } from "@/lib/axios";
+import { CardResponse } from "@/lib/validations/card";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { AxiosError, AxiosResponse } from "axios";
+
+interface useDeleteCardMutationProps {
+  id: string;
+}
+
+export function useDeleteCardMutation({ id }: useDeleteCardMutationProps) {
+  const queryClient = useQueryClient();
+
+  return useMutation<AxiosResponse<CardResponse>, AxiosError>({
+    mutationKey: ["Cards", "Delete"],
+    mutationFn: () => axiosInstance.delete(`/cards/${id}`),
+    onSuccess: (response) =>
+      Promise.all([
+        queryClient.invalidateQueries({
+          queryKey: ["Cards", "GetAll"],
+        }),
+        queryClient.invalidateQueries({
+          queryKey: ["Cards", "Get", id],
+        }),
+        queryClient.invalidateQueries({
+          queryKey: ["Cards", "Get", "Fingerprint", response.data.fingerprint],
+        }),
+      ]),
+  });
+}
