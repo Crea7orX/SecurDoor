f79f0b8cff4f3a72badb673c6493e67b81d4ceb5
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-01-29 21:46:03 +0200

    feat(app): Create use-get-all-logs-actors-query.ts hook

---
+++ app/src/hooks/api/logs/use-get-all-logs-actors-query.ts
@@ -0,0 +1,11 @@
+import { axiosInstance } from "@/lib/axios";
+import { type LogActorResponse } from "@/lib/validations/log";
+import { useQuery } from "@tanstack/react-query";
+import { type AxiosError } from "axios";
+
+export function useGetAllLogsActorsQuery() {
+  return useQuery<LogActorResponse[], AxiosError>({
+    queryKey: ["Logs", "Actors", "GetAll"],
+    queryFn: async () => (await axiosInstance.get("/logs/actors")).data,
+  });
+}
