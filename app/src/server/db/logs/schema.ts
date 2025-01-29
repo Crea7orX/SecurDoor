3ead9e7167e357ef2133390c71662aa5aa706223
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-01-29 22:20:27 +0200

    feat(app): Add columns in logs schema.ts

--- app/src/server/db/logs/schema.ts
+++ app/src/server/db/logs/schema.ts
@@ -4,7 +4,9 @@ import { integer, pgTable, text, varchar } from "drizzle-orm/pg-core";
 export const logs = pgTable("logs", {
   id: varchar("id", { length: 256 }).primaryKey(),
   action: varchar("action", { length: 256 }).notNull(),
-  actor: varchar("actor", { length: 1024 }).notNull(),
+  actorName: varchar("actor_name", { length: 1024 }),
+  actorEmail: varchar("actor_email", { length: 1024 }),
+  actorId: varchar("actor_id", { length: 256 }).notNull(),
   objectId: varchar("object_id", { length: 256 }),
   reference: text("reference")
     .array()
