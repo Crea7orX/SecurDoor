db65903a1669ce12878b27142255579658551cf5
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-01-29 21:43:49 +0200

    feat(app): Update and create logs queries.ts

--- app/src/server/db/logs/queries.ts
+++ app/src/server/db/logs/queries.ts
@@ -1,9 +1,10 @@
 import "server-only";
 import IdPrefix, { generateId } from "@/lib/ids";
+import { type LogsGetSchema } from "@/lib/validations/log";
 import { db } from "@/server/db";
 import { logs } from "@/server/db/logs/schema";
 import { clerkClient } from "@clerk/nextjs/server";
-import { and, desc, eq } from "drizzle-orm";
+import { and, asc, count, desc, eq, inArray } from "drizzle-orm";
 
 export async function logInsert(
   userId: string,
@@ -35,12 +36,52 @@ export async function logInsert(
     });
 }
 
-export function logsGetAll(ownerId: string) {
-  return db
-    .select()
-    .from(logs)
-    .orderBy(desc(logs.createdAt))
-    .where(eq(logs.ownerId, ownerId));
+export async function logsGetAll(input: LogsGetSchema, ownerId: string) {
+  try {
+    const offset = (input.page - 1) * input.perPage;
+
+    const where = and(
+      eq(logs.ownerId, ownerId), // Only show logs for the organization
+      input.action.length > 0 ? inArray(logs.action, input.action) : undefined,
+      input.actor.length > 0 ? inArray(logs.actor, input.actor) : undefined,
+    );
+
+    const orderBy =
+      input.sort.length > 0
+        ? input.sort.map((item) =>
+            item.desc ? desc(logs[item.id]) : asc(logs[item.id]),
+          )
+        : [asc(logs.createdAt)];
+
+    const { data, total } = await db.transaction(async (tx) => {
+      const data = await tx
+        .select()
+        .from(logs)
+        .limit(input.perPage)
+        .offset(offset)
+        .where(where)
+        .orderBy(...orderBy);
+
+      const total = await tx
+        .select({
+          count: count(),
+        })
+        .from(logs)
+        .where(where)
+        .execute()
+        .then((res) => res[0]?.count ?? 0);
+
+      return {
+        data,
+        total,
+      };
+    });
+
+    const pageCount = Math.ceil(total / input.perPage);
+    return { data, pageCount };
+  } catch (error) {
+    return { data: [], pageCount: 0 };
+  }
 }
 
 export async function logGetById(id: string, ownerId: string) {
@@ -53,4 +94,15 @@ export async function logGetById(id: string, ownerId: string) {
         .limit(1)
     )[0] ?? null
   );
+}
+
+export function logsActorsGetAll(ownerId: string) {
+  return db
+    .select({
+      actor: logs.actor,
+    })
+    .from(logs)
+    .where(eq(logs.ownerId, ownerId))
+    .groupBy(logs.actor)
+    .orderBy(asc(logs.actor));
 }
