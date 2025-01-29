7c6618b0ead0d093314b0be11c6cb599d028c5e5
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-01-29 21:48:23 +0200

    feat(app): Create logs-table-columns.tsx component

---
+++ app/src/app/dashboard/logs/_components/logs-table-columns.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import type { LogResponse } from "@/lib/validations/log";
+import { type ColumnDef } from "@tanstack/react-table";
+
+export function getColumns(): ColumnDef<LogResponse>[] {
+  return [
+    {
+      accessorKey: "action",
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: "actor",
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: "createdAt",
+      enableSorting: false,
+      enableHiding: false,
+    },
+  ];
+}
