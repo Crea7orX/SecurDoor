15bc6f5b78fd68ad9e00513e32c09f087cc75bf5
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-01-29 21:42:57 +0200

    feat(app): Add log.ts zod validation schemas

--- app/src/lib/validations/log.ts
+++ app/src/lib/validations/log.ts
@@ -1,3 +1,10 @@
+import { LogDisplayInfos } from "@/config/logs";
+import { getSortingStateParser } from "@/lib/data-table-parsers";
+import {
+  createSearchParamsCache,
+  parseAsArrayOf,
+  parseAsInteger,
+} from "nuqs/server";
 import { z } from "zod";
 
 export const logResponseSchema = z.object({
@@ -11,3 +18,32 @@ export const logResponseSchema = z.object({
 });
 
 export type LogResponse = z.infer<typeof logResponseSchema>;
+
+export const logSearchParamsCache = createSearchParamsCache({
+  page: parseAsInteger.withDefault(1),
+  perPage: parseAsInteger.withDefault(10),
+  sort: getSortingStateParser<LogResponse>().withDefault([
+    { id: "createdAt", desc: true },
+  ]),
+  action: parseAsArrayOf(
+    z.enum(Object.keys(LogDisplayInfos) as [keyof typeof LogDisplayInfos]),
+  ).withDefault([]),
+  actor: parseAsArrayOf(z.string()).withDefault([]),
+});
+
+export type LogsGetSchema = Awaited<
+  ReturnType<typeof logSearchParamsCache.parse>
+>;
+
+export const logsPaginatedResponseSchema = z.object({
+  data: logResponseSchema.array(),
+  pageCount: z.number(),
+});
+
+export type LogsPaginatedResponse = z.infer<typeof logsPaginatedResponseSchema>;
+
+export const logActorResponseSchema = z.object({
+  actor: z.string(),
+});
+
+export type LogActorResponse = z.infer<typeof logActorResponseSchema>;
