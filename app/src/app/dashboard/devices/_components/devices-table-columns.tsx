d3dee80e29853e18e85736a3e4837fa6664331cd
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-01-31 19:50:47 +0200

    feat(app): Create devices-table-columns.tsx component

---
+++ app/src/app/dashboard/devices/_components/devices-table-columns.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { type DeviceResponse } from "@/lib/validations/device";
+import { type ColumnDef } from "@tanstack/react-table";
+
+export function getColumns(): ColumnDef<DeviceResponse>[] {
+  return [
+    {
+      accessorKey: "name",
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: "createdAt",
+      enableSorting: false,
+      enableHiding: false,
+    },
+  ];
+}
