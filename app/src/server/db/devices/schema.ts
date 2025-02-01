b9ea5ace49232dffd7ecb41dacca1971925ad4a5
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-01 19:44:05 +0200

    feat(app): Add default function for id in devices schema.ts

--- app/src/server/db/devices/schema.ts
+++ app/src/server/db/devices/schema.ts
@@ -1,8 +1,11 @@
+import IdPrefix, { generateId } from "@/lib/ids";
 import { sql } from "drizzle-orm";
 import { integer, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const devices = pgTable("devices", {
-  id: varchar("id", { length: 256 }).primaryKey(),
+  id: varchar("id", { length: 256 })
+    .primaryKey()
+    .$default(() => generateId(IdPrefix.DEVICE)),
   name: varchar("name", { length: 256 }).notNull(),
   serialId: uuid("serial_id").notNull(),
   key: varchar("key", { length: 6 }).notNull(),
