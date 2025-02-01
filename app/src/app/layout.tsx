1874ed5db6265f76e5c2135f4b4a89cd97ad0bcb
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-01 20:13:54 +0200

    fix(app): Providers positions

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -19,18 +19,18 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <ClerkProvider>
-      <ApiProvider>
-        <html lang="en" className={`${GeistSans.variable}`}>
-          <NuqsAdapter>
-            <body className="relative min-h-screen bg-background">
+      <html lang="en" className={`${GeistSans.variable}`}>
+        <body className="relative min-h-screen bg-background">
+          <ApiProvider>
+            <NuqsAdapter>
               <div className="absolute -z-50 h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]" />
               {children}
               <Toaster />
               <TailwindIndicator />
-            </body>
-          </NuqsAdapter>
-        </html>
-      </ApiProvider>
+            </NuqsAdapter>
+          </ApiProvider>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
