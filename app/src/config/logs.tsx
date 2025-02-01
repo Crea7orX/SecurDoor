57ac563768217e15e0891fe3a4ceb6f75a00f144
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-01 17:59:27 +0200

    feat(app): Add logs for devices in logs.tsx config

--- app/src/config/logs.tsx
+++ app/src/config/logs.tsx
@@ -1,7 +1,33 @@
-import { LogDisplayInfo } from "@/types";
-import { CircleDot, CircleHelp, CircleMinus, CirclePlus } from "lucide-react";
+import { type LogDisplayInfo } from "@/types";
+import {
+  CircleDot,
+  CircleHelp,
+  CircleMinus,
+  CirclePlus,
+  Diamond,
+  DiamondMinus,
+  DiamondPlus,
+} from "lucide-react";
 
 export const LogDisplayInfos: Record<string, LogDisplayInfo> = {
+  "device.create": {
+    title: "Device created",
+    text: "{actor} created device",
+    icon: DiamondPlus,
+    color: "success",
+  },
+  "device.delete": {
+    title: "Device deleted",
+    text: "{actor} deleted device",
+    icon: DiamondMinus,
+    color: "destructive",
+  },
+  "device.update": {
+    title: "Device updated",
+    text: "{actor} updated device",
+    icon: Diamond,
+    color: "info",
+  },
   "card.create": {
     title: "Card created",
     text: "{actor} created card",
