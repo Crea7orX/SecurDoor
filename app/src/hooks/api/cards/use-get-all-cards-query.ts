0cc80b83933f5bf7afbc6acc35419d916edd12da
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-02 13:25:35 +0200

    feat(app): Add query options as parameters in use-get-all-cards-query.ts hook

--- app/src/hooks/api/cards/use-get-all-cards-query.ts
+++ app/src/hooks/api/cards/use-get-all-cards-query.ts
@@ -2,19 +2,26 @@ import { useParseSearchParams } from "@/hooks/use-parse-search-params";
 import { axiosInstance } from "@/lib/axios";
 import { type CardsPaginatedResponse } from "@/lib/validations/card";
 import { type SearchParams } from "@/types/data-table";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import {
+  keepPreviousData,
+  useQuery,
+  type UseQueryOptions,
+} from "@tanstack/react-query";
 import { type AxiosError } from "axios";
 
-interface UseGetAllCardsQueryProps {
+interface UseGetAllCardsQueryProps
+  extends Partial<UseQueryOptions<CardsPaginatedResponse, AxiosError>> {
   searchParams: SearchParams;
 }
 
 export function useGetAllCardsQuery({
   searchParams,
+  ...options
 }: UseGetAllCardsQueryProps) {
   const search = useParseSearchParams(searchParams);
 
   return useQuery<CardsPaginatedResponse, AxiosError>({
+    ...options,
     queryKey: ["Cards", "GetAll", searchParams],
     queryFn: async () => (await axiosInstance.get(`/cards?${search}`)).data,
     placeholderData: keepPreviousData,
