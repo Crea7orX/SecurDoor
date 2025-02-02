06b7342da4618083775bb6325bc0891115f21a77
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-02 12:46:01 +0200

    feat(app): Add query options as parameters in use-get-all-devices-query.ts hook

--- app/src/hooks/api/devices/use-get-all-devices-query.ts
+++ app/src/hooks/api/devices/use-get-all-devices-query.ts
@@ -2,19 +2,26 @@ import { useParseSearchParams } from "@/hooks/use-parse-search-params";
 import { axiosInstance } from "@/lib/axios";
 import { type DevicesPaginatedResponse } from "@/lib/validations/device";
 import { type SearchParams } from "@/types/data-table";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import {
+  keepPreviousData,
+  useQuery,
+  type UseQueryOptions,
+} from "@tanstack/react-query";
 import { type AxiosError } from "axios";
 
-interface UseGetAllDevicesQueryProps {
+interface UseGetAllDevicesQueryProps
+  extends Partial<UseQueryOptions<DevicesPaginatedResponse, AxiosError>> {
   searchParams: SearchParams;
 }
 
 export function useGetAllDevicesQuery({
   searchParams,
+  ...options
 }: UseGetAllDevicesQueryProps) {
   const search = useParseSearchParams(searchParams);
 
   return useQuery<DevicesPaginatedResponse, AxiosError>({
+    ...options,
     queryKey: ["Devices", "GetAll", searchParams],
     queryFn: async () => (await axiosInstance.get(`/devices?${search}`)).data,
     placeholderData: keepPreviousData,
