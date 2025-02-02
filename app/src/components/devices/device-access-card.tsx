b0ecb8fe9751e574630adb085430317e8e98f5ff
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-02 13:27:31 +0200

    feat(app): Fetch and display dynamic data in device-access-card.tsx

--- app/src/components/devices/device-access-card.tsx
+++ app/src/components/devices/device-access-card.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { AccessDevicesEditDialog } from "@/components/access/devices/access-devices-edit-dialog";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
@@ -8,7 +11,10 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Eye, IdCard, PlusCircle } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useGetAllAccessDevicesQuery } from "@/hooks/api/access/devices/use-get-all-access-devices-query";
+import { IdCard, SlidersHorizontal } from "lucide-react";
 import * as React from "react";
 
 interface DeviceAccessCardProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -19,7 +25,7 @@ const DeviceAccessCard = React.forwardRef<
   HTMLDivElement,
   DeviceAccessCardProps
 >(({ className, id, ...props }, ref) => {
-  // todo: fetch data from api with id
+  const { data, isLoading } = useGetAllAccessDevicesQuery({ id });
 
   return (
     <Card className={className} ref={ref} {...props}>
@@ -33,20 +39,24 @@ const DeviceAccessCard = React.forwardRef<
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <Badge variant="info" className="text-md">
-          <IdCard className="mr-1 size-4" />
-          <span>32 cards</span>
-        </Badge>
+        {isLoading ? (
+          <Skeleton className="h-8 w-28" />
+        ) : (
+          <Badge variant="info" className="text-md">
+            <IdCard className="mr-1 size-4" />
+            <span>{data?.cards.length} cards</span>
+            {/*todo: plural or singular*/}
+          </Badge>
+        )}
+        <Separator className="mt-6 h-1 rounded-xl" />
       </CardContent>
       <CardFooter className="justify-end gap-2 max-md:flex-col">
-        <Button variant="info" className="max-md:w-full">
-          <Eye />
-          <span>View Cards</span>
-        </Button>
-        <Button variant="success" className="max-md:w-full">
-          <PlusCircle />
-          <span>Add Card</span>
-        </Button>
+        <AccessDevicesEditDialog id={id} cards={data?.cards ?? []}>
+          <Button variant="info" className="max-md:w-full" disabled={isLoading}>
+            <SlidersHorizontal />
+            <span>Edit Cards</span>
+          </Button>
+        </AccessDevicesEditDialog>
       </CardFooter>
     </Card>
   );
