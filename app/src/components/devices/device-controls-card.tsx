32fcfa569e5e4387672abc7fd9c1761f1da9e92c
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-03 13:39:40 +0200

    feat(app): Add alert dialogs for emergency states in device-controls-card.tsx component

--- app/src/components/devices/device-controls-card.tsx
+++ app/src/components/devices/device-controls-card.tsx
@@ -1,3 +1,5 @@
+import { DeviceEmergencyAlertDialog } from "@/components/devices/device-emergency-alert-dialog";
+import { DeviceEmergencyClearAlertDialog } from "@/components/devices/device-emergency-clear-alert-dialog";
 import { DeviceSettingsDialog } from "@/components/devices/device-settings-dialog";
 import { Button } from "@/components/ui/button";
 import {
@@ -11,6 +13,7 @@ import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
 import { type DeviceResponse } from "@/lib/validations/device";
 import {
+  Activity,
   BellElectric,
   Construction,
   Lock,
@@ -58,15 +61,35 @@ const DeviceControlsCard = React.forwardRef<
         <Separator className="h-1 rounded-xl" />
         <Label className="text-md">Emergency</Label>
         <div className="flex gap-2 max-md:flex-col">
-          <Button variant="destructive" className="w-full">
-            <Construction />
-            <span>LOCKDOWN</span>
-          </Button>
-          <Button variant="warning" className="w-full">
-            <BellElectric />
-            <span>EVACUATION</span>
-          </Button>
+          <DeviceEmergencyAlertDialog id={device.id} state="lockdown">
+            <Button
+              variant="destructive"
+              className="w-full"
+              disabled={device.emergencyState === "lockdown"}
+            >
+              <Construction />
+              <span>LOCKDOWN</span>
+            </Button>
+          </DeviceEmergencyAlertDialog>
+          <DeviceEmergencyAlertDialog id={device.id} state="evacuation">
+            <Button
+              variant="warning"
+              className="w-full"
+              disabled={device.emergencyState === "evacuation"}
+            >
+              <BellElectric />
+              <span>EVACUATION</span>
+            </Button>
+          </DeviceEmergencyAlertDialog>
         </div>
+        {device.emergencyState && (
+          <DeviceEmergencyClearAlertDialog id={device.id}>
+            <Button variant="info" className="w-full">
+              <Activity />
+              <span>CLEAR</span>
+            </Button>
+          </DeviceEmergencyClearAlertDialog>
+        )}
       </CardContent>
     </Card>
   );
