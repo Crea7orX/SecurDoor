5220ced9c0ab862d32ff261bce063d88b46576f0
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-03 13:37:22 +0200

    feat(app): Create use-update-emergency-mutation.ts hook

---
+++ app/src/hooks/api/emergency/use-update-emergency-mutation.ts
@@ -0,0 +1,35 @@
+import { axiosInstance } from "@/lib/axios";
+import {
+  type EmergencyResponse,
+  type EmergencyUpdate,
+} from "@/lib/validations/emergency";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { type AxiosError, type AxiosResponse } from "axios";
+
+interface UseUpdateEmergencyMutationProps {
+  id: string;
+}
+
+export function useUpdateEmergencyMutation({
+  id,
+}: UseUpdateEmergencyMutationProps) {
+  const queryClient = useQueryClient();
+
+  return useMutation<
+    AxiosResponse<EmergencyResponse>,
+    AxiosError,
+    EmergencyUpdate
+  >({
+    mutationKey: ["Emergency", "Update"],
+    mutationFn: (update) => axiosInstance.post(`/emergency/${id}`, update),
+    onSuccess: () =>
+      Promise.all([
+        queryClient.invalidateQueries({
+          queryKey: ["Emergency", "Get", id],
+        }),
+        queryClient.invalidateQueries({
+          queryKey: ["Devices", "Get", id],
+        }),
+      ]),
+  });
+}
