5c033973b6bcff29e856547b1a17123e95fd26c5
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-05 14:13:20 +0200

    feat(app): Display adoption state in device-status-card.tsx component

--- app/src/components/devices/device-status-card.tsx
+++ app/src/components/devices/device-status-card.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Badge } from "@/components/ui/badge";
 import {
   Card,
@@ -6,6 +8,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { getDeviceStatusDisplayInfo } from "@/config/device-statuses";
+import { useGetDeviceByIdStateQuery } from "@/hooks/api/devices-states/use-get-device-by-id-state-query";
+import { cn } from "@/lib/utils";
 import { CircleDotDashed, EthernetPort } from "lucide-react";
 import * as React from "react";
 
@@ -17,7 +22,11 @@ const DeviceStatusCard = React.forwardRef<
   HTMLDivElement,
   DeviceStatusCardProps
 >(({ className, id, ...props }, ref) => {
-  // todo: fetch data from api with id
+  const { data } = useGetDeviceByIdStateQuery({ id });
+
+  const deviceStatusDisplayInfo = getDeviceStatusDisplayInfo(
+    data?.status ?? "",
+  );
 
   return (
     <Card className={className} ref={ref} {...props}>
@@ -29,10 +38,23 @@ const DeviceStatusCard = React.forwardRef<
         <CardDescription>Device online status</CardDescription>
       </CardHeader>
       <CardContent className="flex flex-col gap-2">
-        <Badge variant="destructive" className="text-md w-fit">
-          <CircleDotDashed className="mr-1 size-4" />
-          <span>Offline</span>
-        </Badge>
+        <div className="flex flex-wrap gap-2">
+          <Badge variant="destructive" className="text-md">
+            <CircleDotDashed className="mr-1 size-4" />
+            <span>Offline</span>
+          </Badge>
+          {data?.status && data?.status !== "adopted" && (
+            <Badge variant={deviceStatusDisplayInfo.color} className="text-md">
+              <deviceStatusDisplayInfo.icon
+                className={cn(
+                  "mr-1 size-4",
+                  `fill-${deviceStatusDisplayInfo.color}`,
+                )}
+              />
+              <span>{deviceStatusDisplayInfo.text}</span>
+            </Badge>
+          )}
+        </div>
         <p>
           Last seen: <span className="text-md">Never</span>
         </p>
