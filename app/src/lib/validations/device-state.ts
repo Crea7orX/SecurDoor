eb4999a63cd1588706ea7152289cdf60646e252a
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-05 14:10:40 +0200

    feat(app): Create device-state.ts zod validation schema

---
+++ app/src/lib/validations/device-state.ts
@@ -0,0 +1,11 @@
+import { deviceStatusEnum } from "@/server/db/devices-states/schema";
+import { z } from "zod";
+
+export const deviceStateResponseSchema = z.object({
+  id: z.string(),
+  status: z.enum(deviceStatusEnum.enumValues),
+  isLocked: z.boolean(),
+  updatedAt: z.number(),
+});
+
+export type DeviceStateResponse = z.infer<typeof deviceStateResponseSchema>;
