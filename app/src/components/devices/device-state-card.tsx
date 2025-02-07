2dce4ef199c99acd820727c3fbd19c0e24518124
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-08 00:40:07 +0200

    feat(app): Display locked state in device-state-card.tsx component

--- app/src/components/devices/device-state-card.tsx
+++ app/src/components/devices/device-state-card.tsx
@@ -6,8 +6,10 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useGetDeviceByIdStateQuery } from "@/hooks/api/devices-states/use-get-device-by-id-state-query";
 import { type DeviceResponse } from "@/lib/validations/device";
-import { BellElectric, Construction, Hand, LockOpen } from "lucide-react";
+import { BellElectric, Construction, Hand, LockOpen, Lock } from "lucide-react";
 import * as React from "react";
 
 interface DeviceStateCardProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -16,6 +18,11 @@ interface DeviceStateCardProps extends React.HTMLAttributes<HTMLDivElement> {
 
 const DeviceStateCard = React.forwardRef<HTMLDivElement, DeviceStateCardProps>(
   ({ className, device, ...props }, ref) => {
+    const { data, isLoading } = useGetDeviceByIdStateQuery({
+      id: device.id,
+      refetchInterval: 5000,
+    });
+
     return (
       <Card className={className} ref={ref} {...props}>
         <CardHeader>
@@ -26,10 +33,19 @@ const DeviceStateCard = React.forwardRef<HTMLDivElement, DeviceStateCardProps>(
           <CardDescription>State of the door lock</CardDescription>
         </CardHeader>
         <CardContent className="flex gap-2">
-          <Badge variant="success" className="text-md">
-            <LockOpen className="mr-1 size-4" />
-            <span>Unlocked</span>
-          </Badge>
+          {isLoading || !data ? (
+            <Skeleton className="h-8 w-32" />
+          ) : data.isLocked ? (
+            <Badge variant="destructive" className="text-md">
+              <Lock className="mr-1 size-4" />
+              <span>Locked</span>
+            </Badge>
+          ) : (
+            <Badge variant="success" className="text-md">
+              <LockOpen className="mr-1 size-4" />
+              <span>Unlocked</span>
+            </Badge>
+          )}
           {device.emergencyState === "lockdown" ? (
             <Badge
               variant="destructive"
