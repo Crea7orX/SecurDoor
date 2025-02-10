5f061ddf3cbea5eabc90c3bc755b6e2ac3a69ebb
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-11 00:21:46 +0200

    feat(app): Use device state from data instead of another http request in device-status-card.tsx component

--- app/src/components/devices/device-status-card.tsx
+++ app/src/components/devices/device-status-card.tsx
@@ -10,29 +10,25 @@ import {
 } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { getDeviceStatusDisplayInfo } from "@/config/device-statuses";
-import { useGetDeviceByIdStateQuery } from "@/hooks/api/devices-states/use-get-device-by-id-state-query";
 import { useNow } from "@/hooks/use-now";
 import { cn } from "@/lib/utils";
+import { type DeviceResponse } from "@/lib/validations/device";
 import { formatDistance } from "date-fns";
 import { CircleDot, CircleDotDashed, EthernetPort } from "lucide-react";
 import * as React from "react";
 
 interface DeviceStatusCardProps extends React.HTMLAttributes<HTMLDivElement> {
-  id: string;
+  device: DeviceResponse;
 }
 
 const DeviceStatusCard = React.forwardRef<
   HTMLDivElement,
   DeviceStatusCardProps
->(({ className, id, ...props }, ref) => {
-  const { data, isLoading } = useGetDeviceByIdStateQuery({
-    id,
-  });
-
+>(({ className, device, ...props }, ref) => {
   const [now] = useNow(5000); // re-render every 5s for device status
 
   const deviceStatusDisplayInfo = getDeviceStatusDisplayInfo(
-    data?.status ?? "",
+    device.state?.status ?? "",
   );
 
   return (
@@ -46,7 +42,7 @@ const DeviceStatusCard = React.forwardRef<
       </CardHeader>
       <CardContent className="flex flex-col gap-2">
         <div className="flex flex-wrap gap-2">
-          {now.getTime() - (data?.lastSeenAt ?? 0) * 1000 > 15000 ? (
+          {now.getTime() - (device.state?.lastSeenAt ?? 0) * 1000 > 15000 ? (
             <Badge variant="destructive" className="text-md">
               <CircleDotDashed className="mr-1 size-4" />
               <span>Offline</span>
@@ -57,7 +53,7 @@ const DeviceStatusCard = React.forwardRef<
               <span>Online</span>
             </Badge>
           )}
-          {data?.status && data?.status !== "adopted" && (
+          {device.state?.status && device.state?.status !== "adopted" && (
             <Badge variant={deviceStatusDisplayInfo.color} className="text-md">
               <deviceStatusDisplayInfo.icon
                 className={cn(
@@ -69,13 +65,13 @@ const DeviceStatusCard = React.forwardRef<
             </Badge>
           )}
         </div>
-        {isLoading ? (
+        {!device.state ? (
           <Skeleton className="h-6 w-48" />
         ) : (
           <p className="text-muted-foreground">
             Last seen:{" "}
-            {data?.lastSeenAt
-              ? formatDistance(new Date(data.lastSeenAt * 1000), now, {
+            {device.state?.lastSeenAt
+              ? formatDistance(new Date(device.state.lastSeenAt * 1000), now, {
                   includeSeconds: true,
                   addSuffix: true,
                 })
