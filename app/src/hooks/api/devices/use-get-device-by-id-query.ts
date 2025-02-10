b28784d10c91599e08b4c5971d182e9f6c55a17a
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-11 00:20:14 +0200

    feat(app): Add query options as parameters in use-get-device-by-id-query.ts hook

--- app/src/hooks/api/devices/use-get-device-by-id-query.ts
+++ app/src/hooks/api/devices/use-get-device-by-id-query.ts
@@ -1,14 +1,19 @@
 import { axiosInstance } from "@/lib/axios";
-import { DeviceResponse } from "@/lib/validations/device";
-import { useQuery } from "@tanstack/react-query";
-import { AxiosError } from "axios";
+import { type DeviceResponse } from "@/lib/validations/device";
+import { useQuery, type UseQueryOptions } from "@tanstack/react-query";
+import { type AxiosError } from "axios";
 
-interface UseGetDeviceByIdQueryProps {
+interface UseGetDeviceByIdQueryProps
+  extends Partial<UseQueryOptions<DeviceResponse, AxiosError>> {
   id: string;
 }
 
-export function useGetDeviceByIdQuery({ id }: UseGetDeviceByIdQueryProps) {
+export function useGetDeviceByIdQuery({
+  id,
+  ...options
+}: UseGetDeviceByIdQueryProps) {
   return useQuery<DeviceResponse, AxiosError>({
+    ...options,
     queryKey: ["Devices", "Get", id],
     queryFn: async () => (await axiosInstance.get(`/devices/${id}`)).data,
   });
