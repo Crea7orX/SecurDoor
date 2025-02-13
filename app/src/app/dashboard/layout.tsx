5fbffc62a9453cdbf8ecafad99e18076e5766705
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-14 01:54:46 +0200

    feat(app): Protect whole /dashboard layout.tsx

--- app/src/app/dashboard/layout.tsx
+++ app/src/app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import { DashboardSidebar } from "@/components/dashboard/dashboard-sidebar";
 import { Header } from "@/components/header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 import * as React from "react";
 
 export default function DashboardLayout({
@@ -9,12 +10,19 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <SidebarProvider>
-      <DashboardSidebar />
-      <SidebarInset className="bg-transparent">
-        <Header />
-        {children}
-      </SidebarInset>
-    </SidebarProvider>
+    <>
+      <SignedIn>
+        <SidebarProvider>
+          <DashboardSidebar />
+          <SidebarInset className="bg-transparent">
+            <Header />
+            {children}
+          </SidebarInset>
+        </SidebarProvider>
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </>
   );
 }
