e0a9cdc59acdf5e881fd297a431433364e128021
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-13 11:07:26 +0200

    feat(app): Create accessCardTryAuthentication function in cards-to-devices db queries.ts

--- app/src/server/db/cards-to-devices/queries.ts
+++ app/src/server/db/cards-to-devices/queries.ts
@@ -5,13 +5,17 @@ import { db } from "@/server/db";
 import { cardsToDevices } from "@/server/db/cards-to-devices/schema";
 import { cardGetById } from "@/server/db/cards/queries";
 import { cards } from "@/server/db/cards/schema";
-import { deviceGetById } from "@/server/db/devices/queries";
+import { deviceStateHeartbeatUpdate } from "@/server/db/devices-states/queries";
+import {
+  deviceGetById,
+  type deviceGetBySerialIdUnprotected,
+} from "@/server/db/devices/queries";
 import { devices } from "@/server/db/devices/schema";
 import {
   logInsertMultiple,
   type LogsInsertMultipleData,
 } from "@/server/db/logs/queries";
-import { and, asc, eq, inArray } from "drizzle-orm";
+import { and, asc, eq, inArray, sql } from "drizzle-orm";
 
 export function accessDeviceGetAll(deviceId: string, ownerId: string) {
   return db
@@ -278,4 +282,133 @@ export async function accessCardUpdate(
   }
 
   return [newDeviceIds, toDelete];
+}
+
+interface AccessCardTryAuthentication {
+  ownerId: string;
+  device: NonNullable<
+    Awaited<ReturnType<typeof deviceGetBySerialIdUnprotected>>
+  >;
+  deviceId: string;
+  fingerprint: string;
+}
+
+export async function accessCardTryAuthentication({
+  ownerId,
+  device,
+  deviceId,
+  fingerprint,
+}: AccessCardTryAuthentication) {
+  void deviceStateHeartbeatUpdate({ deviceId }); // update device heartbeat
+
+  const access = (
+    await db
+      .select()
+      .from(cardsToDevices)
+      .innerJoin(
+        cards,
+        and(
+          eq(cards.ownerId, ownerId), // Ensure card ownership
+          eq(cards.id, cardsToDevices.cardId),
+        ),
+      )
+      .where(
+        and(
+          eq(cardsToDevices.deviceId, deviceId),
+          eq(cards.fingerprint, fingerprint),
+        ),
+      )
+  )[0];
+
+  // no access given to the device
+  if (!access) throw new ForbiddenError();
+
+  // device in emergency state
+  if (device.emergencyState) throw new ForbiddenError();
+
+  // card is deactivated
+  if (!access.cards.active) throw new ForbiddenError();
+
+  // unlock
+  if (device.isLocked) {
+    await db
+      .update(devices)
+      .set({
+        isLocked: false,
+        updatedAt: sql`(EXTRACT(EPOCH FROM NOW()))`,
+      })
+      .where(
+        and(
+          eq(devices.ownerId, ownerId), // Ensure device ownership
+          eq(devices.id, deviceId),
+        ),
+      );
+
+    // log
+    const logs = [
+      {
+        action: "device.unlock",
+        objectId: deviceId,
+        reference: [
+          device.serialId,
+          access.cards.fingerprint,
+          access.cards.holder ?? "NULL",
+        ],
+      },
+      {
+        action: "card.unlock",
+        objectId: access.cards.id,
+        reference: [
+          access.cards.fingerprint,
+          access.cards.holder ?? "NULL",
+          device.serialId,
+        ],
+      },
+    ];
+    void logInsertMultiple(ownerId, logs, "system");
+  }
+  // lock
+  else {
+    await db
+      .update(devices)
+      .set({
+        isLocked: true,
+        updatedAt: sql`(EXTRACT(EPOCH FROM NOW()))`,
+      })
+      .where(
+        and(
+          eq(devices.ownerId, ownerId), // Ensure device ownership
+          eq(devices.id, deviceId),
+        ),
+      );
+
+    // log
+    const logs = [
+      {
+        action: "device.lock",
+        objectId: deviceId,
+        reference: [
+          device.serialId,
+          access.cards.fingerprint,
+          access.cards.holder ?? "NULL",
+        ],
+      },
+      {
+        action: "card.lock",
+        objectId: access.cards.id,
+        reference: [
+          access.cards.fingerprint,
+          access.cards.holder ?? "NULL",
+          device.serialId,
+        ],
+      },
+    ];
+    void logInsertMultiple(ownerId, logs, "system");
+  }
+
+  return {
+    isLocked: !device.isLocked,
+    reLockDelay: device.reLockDelay,
+    holder: access.cards.holder,
+  };
 }
