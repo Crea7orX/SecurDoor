04699db136f18fe0f565b30b1a5aca85b511f796
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-17 02:19:50 +0200

    feat(app): Create card-add-log-button.tsx component

---
+++ app/src/components/cards/add/card-add-log-button.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/utils";
+import { type LogResponse } from "@/lib/validations/log";
+import * as React from "react";
+
+interface CardAddLogButtonProps extends React.HTMLAttributes<HTMLDivElement> {
+  cardFingerprint: string;
+  setCardFingerprint: (cardFingerprint: string) => void;
+  log: LogResponse;
+}
+
+const CardAddLogButton = React.forwardRef<
+  HTMLDivElement,
+  CardAddLogButtonProps
+>(({ className, cardFingerprint, setCardFingerprint, log, ...props }, ref) => {
+  return (
+    <Card className={cn(className)} ref={ref} {...props}>
+      <CardContent className="flex items-center justify-between p-4">
+        <div>
+          <p>
+            FID:{" "}
+            <span className="font-semibold">
+              {(log.reference![1] as string).slice(-8)}
+            </span>
+          </p>
+          <p>
+            Date:{" "}
+            <span className="font-semibold">
+              {new Date(log.createdAt * 1000).toLocaleString()}
+            </span>
+          </p>
+        </div>
+        <Button
+          onClick={() => setCardFingerprint(log.reference![1] as string)}
+          disabled={cardFingerprint === log.reference![1]}
+        >
+          {cardFingerprint === log.reference![1] ? "Selected" : "Select card"}
+        </Button>
+      </CardContent>
+    </Card>
+  );
+});
+CardAddLogButton.displayName = "CardAddLogButton";
+
+const CardAddLogButtonSkeleton = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <Card className={cn(className)} ref={ref} {...props}>
+      <CardContent className="flex items-center justify-between p-4">
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-6 w-32" />
+          <Skeleton className="h-6 w-48" />
+        </div>
+        <Skeleton className="h-9 w-28" />
+      </CardContent>
+    </Card>
+  );
+});
+CardAddLogButtonSkeleton.displayName = "CardAddLogButtonSkeleton";
+
+export { CardAddLogButton, CardAddLogButtonSkeleton };
