b1536db0142d20274ba6313c00eb887764069c9d
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-26 10:15:26 +0200

    feat(app): Add translation keys in card-added-card.tsx component

--- app/src/components/cards/card-added-card.tsx
+++ app/src/components/cards/card-added-card.tsx
@@ -8,6 +8,7 @@ import {
 } from "@/components/ui/card";
 import { type CardResponse } from "@/lib/validations/card";
 import { CalendarClock } from "lucide-react";
+import { useTranslations } from "next-intl";
 import * as React from "react";
 
 interface CardAddedCardProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -16,16 +17,16 @@ interface CardAddedCardProps extends React.HTMLAttributes<HTMLDivElement> {
 
 const CardAddedCard = React.forwardRef<HTMLDivElement, CardAddedCardProps>(
   ({ className, card, ...props }, ref) => {
+    const t = useTranslations("Card.added");
+
     return (
       <Card className={className} ref={ref} {...props}>
         <CardHeader>
           <CardTitle className="inline-flex items-center gap-1">
             <CalendarClock className="size-6" />
-            <span>Added</span>
+            <span>{t("title")}</span>
           </CardTitle>
-          <CardDescription>
-            The date and time when the card was added
-          </CardDescription>
+          <CardDescription>{t("description")}</CardDescription>
         </CardHeader>
         <CardContent>
           <Badge variant="info" className="text-md">
