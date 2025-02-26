213c31de80f79c409810d1a7f717c283f34ac48f
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-26 10:06:12 +0200

    feat(app): Add namespaces in i18n config.ts

--- app/src/i18n/config.ts
+++ app/src/i18n/config.ts
@@ -3,4 +3,11 @@ export type Locale = (typeof locales)[number];
 export const locales = ["en", "bg"] as const;
 export const defaultLocale: Locale = "en";
 
-export const namespaces = [];
+export const namespaces = [
+  "card",
+  "common",
+  "data-table",
+  "device",
+  "log",
+  "navigation",
+];
