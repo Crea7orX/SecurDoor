c22a7ec554e726622c4d992d194aea600712e309
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-26 10:19:21 +0200

    feat(app): Add translation keys in dashboard-sidebar-navigation.tsx component

--- app/src/components/dashboard/dashboard-sidebar-navigation.tsx
+++ app/src/components/dashboard/dashboard-sidebar-navigation.tsx
@@ -6,6 +6,7 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import { type Navigation } from "@/types";
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 
 interface DashboardSidebarNavigationProps {
@@ -15,6 +16,8 @@ interface DashboardSidebarNavigationProps {
 export function DashboardSidebarNavigation({
   items,
 }: DashboardSidebarNavigationProps) {
+  const _t = useTranslations();
+
   return (
     <SidebarGroup>
       <SidebarGroupLabel>Dashboard</SidebarGroupLabel>
@@ -22,13 +25,13 @@ export function DashboardSidebarNavigation({
         {items.map((item, index) => (
           <SidebarMenuItem key={index}>
             <SidebarMenuButton
-              tooltip={item.title}
+              tooltip={_t(item.title)}
               isActive={item.active}
               asChild
             >
               <Link href={item.url}>
                 {item.icon && <item.icon />}
-                <span>{item.title}</span>
+                <span>{_t(item.title)}</span>
               </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
