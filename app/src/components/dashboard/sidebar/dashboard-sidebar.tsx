a04eacf29970d9ba3b082370f8542768bb3a3389
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-02-28 22:59:07 +0200

    fix(app): Logo home page link in dashboard-sidebar.tsx component

--- app/src/components/dashboard/sidebar/dashboard-sidebar.tsx
+++ app/src/components/dashboard/sidebar/dashboard-sidebar.tsx
@@ -27,7 +27,7 @@ export function DashboardSidebar({
   return (
     <Sidebar className="border-r" collapsible="none" {...props}>
       <SidebarHeader className="h-12 items-center justify-center border-b">
-        <Link href="/app/public">
+        <Link href="/">
           <Logo className="h-10" />
         </Link>
       </SidebarHeader>
