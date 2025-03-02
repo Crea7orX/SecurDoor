304fecc82c28aa5ea875e17b45b6ea45940a1120
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-03-02 20:13:16 +0200

    style(app): Make cards loading.tsx component responsive

--- app/src/app/dashboard/cards/loading.tsx
+++ app/src/app/dashboard/cards/loading.tsx
@@ -9,7 +9,7 @@ export default function CardsLoading() {
   const t = useTranslations("Card.add");
 
   return (
-    <div className="flex flex-col items-center justify-center gap-6 p-4">
+    <div className="flex flex-col items-center justify-center gap-6 p-1 sm:p-4">
       <div className="flex w-full items-center justify-between gap-2 overflow-auto p-1">
         <div className="flex flex-1 items-center gap-2">
           <Skeleton className="h-8 w-40 lg:w-64" />
