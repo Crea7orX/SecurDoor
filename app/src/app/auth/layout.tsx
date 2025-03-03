43576b02272a501a5bde98d10c1ec9d392ebde66
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-03-03 15:10:02 +0200

    style(app): Auth layout.tsx grow

--- app/src/app/auth/layout.tsx
+++ app/src/app/auth/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex flex-1 flex-col items-center justify-center">
       {children}
     </main>
   );
