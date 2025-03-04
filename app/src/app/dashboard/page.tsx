1b31399eb0fce6489dbe01ac7435fa0351f284cd
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-03-05 01:48:31 +0200

    feat(app): Create dashboard page.tsx

--- app/src/app/dashboard/page.tsx
+++ app/src/app/dashboard/page.tsx
@@ -1,3 +1,27 @@
+import { ChartAccessForWeek } from "@/components/dashboard/charts/access/chart-access-for-week";
+import { ChartDashboardAccessForWeek } from "@/components/dashboard/charts/access/chart-dashboard-access-for-week";
+import { DashboardRecentAccessCard } from "@/components/dashboard/dashboard-recent-access-card";
+import { DeviceEmergencyCountAlert } from "@/components/devices/device-emergency-count-alert";
+import { Card } from "@/components/ui/card";
+
 export default function DashboardPage() {
-  return <main>This is the dashboard page</main>;
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <DeviceEmergencyCountAlert href="/dashboard/devices?emergencyState=lockdown,evacuation" />
+      <div className="flex h-full flex-1 gap-6">
+        <DashboardRecentAccessCard className="w-full max-w-96 max-lg:hidden" />
+
+        <div className="flex flex-1 flex-col gap-4">
+          <Card className="h-full w-full bg-border px-2 py-4">
+            <div className="sticky top-0 flex flex-wrap justify-center gap-4">
+              <ChartDashboardAccessForWeek className="2xl:w-[49%]" />
+              <ChartAccessForWeek className="2xl:w-[49%]" />
+            </div>
+          </Card>
+
+          <DashboardRecentAccessCard className="lg:hidden" />
+        </div>
+      </div>
+    </div>
+  );
 }
