7eb283beb690bf44695765c94151c05d9d931d3b
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-03-06 04:11:09 +0200

    feat(app): Set default active chart to locks in chart-access-for-week.tsx component

--- app/src/components/dashboard/charts/access/chart-access-for-week.tsx
+++ app/src/components/dashboard/charts/access/chart-access-for-week.tsx
@@ -39,7 +39,7 @@ const ChartAccessForWeek = React.forwardRef<
   });
 
   const [activeChart, setActiveChart] =
-    React.useState<keyof ChartConfig>("unlocks");
+    React.useState<keyof ChartConfig>("locks");
 
   return (
     <Card className={cn("w-full", className)} ref={ref} {...props}>
