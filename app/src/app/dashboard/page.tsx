3af18303ee813f5274bb81a7f3090510a490ab1d
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-03-06 02:06:45 +0200

    feat(app): Add active users charts in dashboard page.tsx

--- app/src/app/dashboard/page.tsx
+++ app/src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 import { ChartAccessForWeek } from "@/components/dashboard/charts/access/chart-access-for-week";
 import { ChartDashboardAccessForWeek } from "@/components/dashboard/charts/access/chart-dashboard-access-for-week";
+import { ChartActiveUsersForWeek } from "@/components/dashboard/charts/active-users/chart-active-users-for-week";
+import { ChartDashboardActiveUsersForWeek } from "@/components/dashboard/charts/active-users/chart-dashboard-active-users-for-week";
 import { DashboardRecentAccessCard } from "@/components/dashboard/dashboard-recent-access-card";
 import { DeviceEmergencyCountAlert } from "@/components/devices/device-emergency-count-alert";
 import { Card } from "@/components/ui/card";
@@ -16,6 +18,8 @@ export default function DashboardPage() {
             <div className="sticky top-0 flex flex-wrap justify-center gap-4">
               <ChartDashboardAccessForWeek className="2xl:w-[49%]" />
               <ChartAccessForWeek className="2xl:w-[49%]" />
+              <ChartDashboardActiveUsersForWeek className="2xl:w-[49%]" />
+              <ChartActiveUsersForWeek className="2xl:w-[49%]" />
             </div>
           </Card>
 
