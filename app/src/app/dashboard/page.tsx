daccdbbbcdb2cc4f9ea8779f77681fb949529962
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-04-04 13:40:47 +0300

    feat(app): Add bulk control button to dashboard page.tsx

--- app/src/app/dashboard/page.tsx
+++ app/src/app/dashboard/page.tsx
@@ -5,10 +5,17 @@ import { ChartDashboardActiveUsersForWeek } from "@/components/dashboard/charts/
 import { ChartEmergencyForWeek } from "@/components/dashboard/charts/emergency/chart-emergency-for-week";
 import { DashboardRecentAccessCard } from "@/components/dashboard/dashboard-recent-access-card";
 import { DemoAlert } from "@/components/demo/demo-alert";
+import { DeviceBulkControlDialog } from "@/components/devices/bulk/device-bulk-control-dialog";
 import { DeviceEmergencyCountAlert } from "@/components/devices/device-emergency-count-alert";
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { SquareStack } from "lucide-react";
+import { useTranslations } from "next-intl";
+import * as React from "react";
 
 export default function DashboardPage() {
+  const t = useTranslations("Device.bulk");
+
   return (
     <div className="flex h-full flex-col gap-4 p-4">
       <DemoAlert />
@@ -18,6 +25,14 @@ export default function DashboardPage() {
         <DashboardRecentAccessCard className="w-full max-w-96 max-lg:hidden" />
 
         <div className="flex flex-1 flex-col gap-4">
+          <Card className="w-full items-end bg-border px-2 py-4">
+            <DeviceBulkControlDialog>
+              <Button className="max-sm:w-full">
+                <SquareStack className="size-4" />
+                {t("button")}
+              </Button>
+            </DeviceBulkControlDialog>
+          </Card>
           <Card className="h-full w-full bg-border px-2 py-4">
             <div className="sticky top-0 grid justify-center gap-4 2xl:grid-cols-2">
               <ChartDashboardAccessForWeek />
