0cb798f3a3ea19c0c97e2694ebef8e671a1a66fc
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-04-11 10:55:37 +0300

    feat(app): Use NEXT_PUBLIC_API_BASE_URL env variable in axios.ts utils

--- app/src/lib/axios.ts
+++ app/src/lib/axios.ts
@@ -1,12 +1,9 @@
 "use client";
 
+import { env } from "@/env";
 import axios from "axios";
 
-axios.defaults.baseURL =
-  process.env.NODE_ENV === "production"
-    ? "https://www.securdoor.eu/api"
-    : "http://localhost:3000/api";
-
+axios.defaults.baseURL = env.NEXT_PUBLIC_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 export const axiosInstance = axios.create();
