10a1f48c9c60a5372cba4dce5b78752f9025fbb0
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-04-18 01:28:22 +0300

    feat(app): Create /api/api_keys GET and POST route.ts

---
+++ app/src/app/api/api_keys/route.ts
@@ -0,0 +1,40 @@
+import { authenticate } from "@/lib/auth";
+import { handleError } from "@/lib/errors";
+import {
+  type ApiKeyCreate,
+  apiKeyCreateSchema,
+  apiKeyResponseSchema,
+} from "@/lib/validations/api-key";
+import { apiKeysGetAll, apiKeysInsert } from "@/server/db/api-keys/queries";
+import { type NextRequest, NextResponse } from "next/server";
+
+// get api keys
+export async function GET(request: NextRequest) {
+  try {
+    const { ownerId } = await authenticate(request);
+
+    const apiKeys = await apiKeysGetAll({ ownerId });
+
+    return NextResponse.json(apiKeyResponseSchema.array().parse(apiKeys));
+  } catch (error) {
+    return handleError(error);
+  }
+}
+
+// create api key
+export async function POST(request: NextRequest) {
+  try {
+    const { userId, ownerId } = await authenticate(request);
+
+    const json = (await request.json()) as ApiKeyCreate;
+    const create = apiKeyCreateSchema.parse(json);
+
+    const apiKey = await apiKeysInsert({ create, userId, ownerId });
+
+    return NextResponse.json(apiKeyResponseSchema.parse(apiKey), {
+      status: 201,
+    });
+  } catch (error) {
+    return handleError(error);
+  }
+}
