ab1fdb2df13c28000ad0a1504c45705c5c878404
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-04-18 02:57:30 +0300

    feat(app): Create api-keys-list.tsx skeleton component

--- app/src/components/api-keys/api-keys-list.tsx
+++ app/src/components/api-keys/api-keys-list.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import { ApiKeyCard } from "@/components/api-keys/api-key-card";
+import {
+  ApiKeyCard,
+  ApiKeyCardSkeleton,
+} from "@/components/api-keys/api-key-card";
+import { NoResultsLabel } from "@/components/data-table/no-results-label";
 import { useGetAllApiKeysQuery } from "@/hooks/api/api-keys/use-get-all-api-keys-query";
 import { cn } from "@/lib/utils";
 import * as React from "react";
@@ -14,6 +18,18 @@ export function ApiKeysList({
   return (
     <div className={cn("flex flex-col gap-1", className)} {...props}>
       {data?.map((apiKey) => <ApiKeyCard apiKey={apiKey} key={apiKey.id} />)}
+    </div>
+  );
+}
+
+export function ApiKeysListSkeleton({
+  className,
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-1", className)}>
+      {Array.from({ length: 3 }).map((_, index) => (
+        <ApiKeyCardSkeleton key={index} />
+      ))}
     </div>
   );
 }
