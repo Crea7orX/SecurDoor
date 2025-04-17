9f3bc16d34530e767edc6cfb4570b914491ef13e
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-04-18 02:32:38 +0300

    feat(app): Add component props for api-keys-list.tsx

--- app/src/components/api-keys/api-keys-list.tsx
+++ app/src/components/api-keys/api-keys-list.tsx
@@ -2,12 +2,17 @@
 
 import { ApiKeyCard } from "@/components/api-keys/api-key-card";
 import { useGetAllApiKeysQuery } from "@/hooks/api/api-keys/use-get-all-api-keys-query";
+import { cn } from "@/lib/utils";
+import * as React from "react";
 
-export function ApiKeysList() {
+export function ApiKeysList({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
   const { data, isLoading } = useGetAllApiKeysQuery({});
 
   return (
-    <div className="flex flex-col gap-1">
+    <div className={cn("flex flex-col gap-1", className)} {...props}>
       {data?.map((apiKey) => <ApiKeyCard apiKey={apiKey} key={apiKey.id} />)}
     </div>
   );
