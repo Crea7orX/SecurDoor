54f14c2c4afbf84c9d266dcdbbae2913355b99e8
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-04-20 02:13:19 +0300

    feat(app): Add card.ts tags zod validation schemas

--- app/src/lib/validations/card.ts
+++ app/src/lib/validations/card.ts
@@ -1,4 +1,5 @@
 import { getSortingStateParser } from "@/lib/data-table-parsers";
+import { tagResponseSchema } from "@/lib/validations/tag";
 import {
   createSearchParamsCache,
   parseAsArrayOf,
@@ -57,4 +58,30 @@ export const cardsPaginatedResponseSchema = z.object({
 
 export type CardsPaginatedResponse = z.infer<
   typeof cardsPaginatedResponseSchema
+>;
+
+export const cardTagsResponseSchema = z.object({
+  tags: z.array(
+    tagResponseSchema.pick({
+      id: true,
+      name: true,
+    }),
+  ),
+});
+
+export type CardTagsResponse = z.infer<typeof cardTagsResponseSchema>;
+
+export const cardTagsUpdateSchema = z.object({
+  tags: z.string().array(),
+});
+
+export type CardTagsUpdate = z.infer<typeof cardTagsUpdateSchema>;
+
+export const cardTagsUpdateResponseSchema = z.object({
+  addedTags: z.string().array(),
+  removedTags: z.string().array(),
+});
+
+export type CardTagsUpdateResponse = z.infer<
+  typeof cardTagsUpdateResponseSchema
 >;
