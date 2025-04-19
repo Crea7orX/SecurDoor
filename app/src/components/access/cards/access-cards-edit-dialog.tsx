6ccbd151922bdfdb2ea936bb503a25c892c2bfc7
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-04-20 02:15:49 +0300

    feat(app): Add tags update in access-cards-edit-dialog.tsx component

--- app/src/components/access/cards/access-cards-edit-dialog.tsx
+++ app/src/components/access/cards/access-cards-edit-dialog.tsx
@@ -24,7 +24,9 @@ import {
 } from "@/components/ui/multi-select";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useUpdateAccessCardMutation } from "@/hooks/api/access/cards/use-update-access-card-mutation";
+import { useUpdateCardTagsMutation } from "@/hooks/api/cards/tags/use-update-card-tags-mutation";
 import { useGetAllDevicesQuery } from "@/hooks/api/devices/use-get-all-devices-query";
+import { useGetAllTagsQuery } from "@/hooks/api/tags/use-get-all-tags-query";
 import { useI18nZodErrors } from "@/hooks/use-i18n-zod-errors";
 import { arraysEquals } from "@/lib/utils";
 import {
@@ -32,6 +34,11 @@ import {
   type AccessCardUpdate,
   accessCardUpdateSchema,
 } from "@/lib/validations/access";
+import {
+  type CardTagsResponse,
+  type CardTagsUpdate,
+  cardTagsUpdateSchema,
+} from "@/lib/validations/card";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useTranslations } from "next-intl";
 import * as React from "react";
@@ -42,12 +49,14 @@ interface AccessCardsEditDialogProps
   extends React.ComponentPropsWithoutRef<typeof Dialog> {
   id: string;
   devices: Pick<AccessCardResponse, "devices">["devices"];
+  tags: Pick<CardTagsResponse, "tags">["tags"];
 }
 
 export function AccessCardsEditDialog({
   children,
   id,
   devices,
+  tags,
   ...props
 }: AccessCardsEditDialogProps) {
   const t = useTranslations("Card");
@@ -55,35 +64,64 @@ export function AccessCardsEditDialog({
 
   const [isOpen, setIsOpen] = React.useState(false);
 
-  const { data, isLoading: isLoadingData } = useGetAllDevicesQuery({
-    searchParams: {
-      perPage: "50", // todo: maybe pagination
-    },
-    enabled: isOpen,
-  });
+  const { data: devicesData, isLoading: devicesIsLoading } =
+    useGetAllDevicesQuery({
+      searchParams: {
+        perPage: "50", // todo: maybe pagination
+        sort: '[{"id":"name","desc":false},{"id":"createdAt","desc":false}]',
+      },
+      enabled: isOpen,
+    });
   const deviceOptions: MultiSelectOption[] | undefined = React.useMemo(() => {
-    return data?.data.map((device) => {
+    return devicesData?.data.map((device) => {
       return {
         label: device.name,
         value: device.name + "-" + device.id,
         key: device.id,
       };
     });
-  }, [data]);
+  }, [devicesData]);
 
-  const { mutateAsync: update } = useUpdateAccessCardMutation({
+  const { data: tagsData, isLoading: tagsIsLoading } = useGetAllTagsQuery({
+    searchParams: {
+      perPage: "50", // todo: maybe pagination
+      sort: '[{"id":"name","desc":false},{"id":"createdAt","desc":false}]',
+    },
+    enabled: isOpen,
+  });
+  const tagOptions: MultiSelectOption[] | undefined = React.useMemo(() => {
+    return tagsData?.data.map((tag) => {
+      return {
+        label: t("access.dialog.tag", {
+          name: tag.name,
+        }),
+        value: tag.name + "-" + tag.id,
+        key: tag.id,
+      };
+    });
+  }, [tagsData, t]);
+
+  const { mutateAsync: accessUpdate } = useUpdateAccessCardMutation({
+    id,
+  });
+  const { mutateAsync: tagsUpdate } = useUpdateCardTagsMutation({
     id,
   });
   const [isLoading, setIsLoading] = React.useState(false);
 
   useI18nZodErrors();
-  const form = useForm<AccessCardUpdate>({
-    resolver: zodResolver(accessCardUpdateSchema),
+  const form = useForm<AccessCardUpdate & CardTagsUpdate>({
+    resolver: zodResolver(
+      accessCardUpdateSchema.extend(cardTagsUpdateSchema.shape),
+    ),
     defaultValues: {
       devices: devices.map((device) => device.id),
+      tags: tags.map((tag) => tag.id),
     },
+    disabled: isLoading,
   });
   form.watch("devices");
+  form.watch("tags");
 
   // Set devices value on devices update
   /* eslint-disable react-hooks/exhaustive-deps */
@@ -94,26 +132,45 @@ export function AccessCardsEditDialog({
     );
   }, [devices]);
 
+  // Set tags value on tags update
+  /* eslint-disable react-hooks/exhaustive-deps */
+  React.useEffect(() => {
+    form.setValue(
+      "tags",
+      tags.map((tag) => tag.id),
+    );
+  }, [tags]);
+
   const resetForm = () => {
     form.reset();
     form.setValue(
       "devices",
       devices.map((device) => device.id),
     );
+    form.setValue(
+      "tags",
+      tags.map((tag) => tag.id),
+    );
   };
 
-  const onSubmit = async (data: AccessCardUpdate) => {
-    if (
-      arraysEquals(
-        devices.map((device) => device.id),
-        data.devices,
-      )
-    )
-      return;
+  const sameDevicesSelected = arraysEquals(
+    devices.map((device) => device.id).sort(),
+    form.getValues("devices").sort(),
+  );
+  const sameTagsSelected = arraysEquals(
+    tags.map((tag) => tag.id).sort(),
+    form.getValues("tags").sort(),
+  );
+
+  const onSubmit = async (data: AccessCardUpdate & CardTagsUpdate) => {
+    if (sameDevicesSelected && sameTagsSelected) return;
 
     setIsLoading(true);
     const toastId = toast.loading(t("access.dialog.notification.loading"));
-    await update(data)
+    await Promise.all([
+      !sameDevicesSelected && accessUpdate({ devices: data.devices }),
+      !sameTagsSelected && tagsUpdate({ tags: data.tags }),
+    ])
       .then(() => {
         toast.success(t("access.dialog.notification.success"), {
           id: toastId,
@@ -148,7 +205,7 @@ export function AccessCardsEditDialog({
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>{t("field.devices.label")}</FormLabel>
-                  {isLoadingData ? (
+                  {devicesIsLoading ? (
                     <Skeleton className="h-10 w-full" />
                   ) : (
                     <FormControl>
@@ -170,17 +227,41 @@ export function AccessCardsEditDialog({
                 </FormItem>
               )}
             />
+            <FormField
+              control={form.control}
+              name="tags"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{t("field.tags.label")}</FormLabel>
+                  {tagsIsLoading ? (
+                    <Skeleton className="h-10 w-full" />
+                  ) : (
+                    <FormControl>
+                      <MultiSelect
+                        {...field}
+                        options={tagOptions ?? []}
+                        value={tagOptions?.filter((option) =>
+                          field.value.includes(option.key),
+                        )}
+                        onChange={(options) => {
+                          field.onChange(options.map((option) => option.key));
+                        }}
+                        placeholder={t("field.tags.placeholder")}
+                        heading={t("field.tags.label")}
+                      />
+                    </FormControl>
+                  )}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
             <DialogFooter className="gap-y-2">
               <Button
                 variant="outline"
                 type="button"
                 onClick={resetForm}
                 disabled={
-                  isLoading ||
-                  arraysEquals(
-                    devices.map((device) => device.id).sort(),
-                    form.getValues("devices").sort(),
-                  )
+                  isLoading || (sameDevicesSelected && sameTagsSelected)
                 }
               >
                 {tButton("reset")}
@@ -188,10 +269,14 @@ export function AccessCardsEditDialog({
               <Button
                 disabled={
                   isLoading ||
-                  arraysEquals(
+                  (arraysEquals(
                     devices.map((device) => device.id).sort(),
                     form.getValues("devices").sort(),
-                  )
+                  ) &&
+                    arraysEquals(
+                      tags.map((tag) => tag.id).sort(),
+                      form.getValues("tags").sort(),
+                    ))
                 }
               >
                 {tButton("save_changes")}
