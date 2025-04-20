0efb7de27d670c7c9bec3423b333327a1b610373
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-04-20 13:13:22 +0300

    feat(app): Add devices count column in tags-table-columns.tsx config

--- app/src/app/dashboard/tags/_components/tags-table-columns.tsx
+++ app/src/app/dashboard/tags/_components/tags-table-columns.tsx
@@ -4,12 +4,16 @@ import { DataTableColumnHeader } from "@/components/data-table/data-table-column
 import { FormattedDate } from "@/components/data-table/formatted-date";
 import { TagActions } from "@/components/tags/tag-actions";
 import { Badge } from "@/components/ui/badge";
-import { type TagResponse } from "@/lib/validations/tag";
+import {
+  type TagResponse,
+  type TagResponseExpand,
+} from "@/lib/validations/tag";
 import { type ColumnDef } from "@tanstack/react-table";
+import { Microchip } from "lucide-react";
 
 const translationKey = "Tag.filter";
 
-export function getColumns(): ColumnDef<TagResponse>[] {
+export function getColumns(): ColumnDef<TagResponse & TagResponseExpand>[] {
   return [
     {
       accessorKey: "name",
@@ -29,6 +33,33 @@ export function getColumns(): ColumnDef<TagResponse>[] {
       ),
       meta: {
         headerName: `${translationKey}.name.label`,
+      },
+    },
+    {
+      accessorKey: "devicesCount",
+      enableSorting: false,
+      enableHiding: false,
+      header: ({ column }) => (
+        <DataTableColumnHeader
+          column={column}
+          title={`${translationKey}.devices_count.label`}
+        />
+      ),
+      cell: ({ row }) => {
+        const devicesCount = Number(row.getValue("devicesCount"));
+
+        return (
+          <Badge
+            variant={devicesCount == 0 ? "warning" : "success"}
+            className="text-md"
+          >
+            <Microchip className="mr-1 size-4" />
+            <span>{devicesCount}</span>
+          </Badge>
+        );
+      },
+      meta: {
+        headerName: `${translationKey}.devices_count.label`,
       },
     },
     {
