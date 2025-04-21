debcfcbee23435be395600e0e2ca6e6f09456fa5
Crea7orX <90335627+Crea7orX@users.noreply.github.com>
2025-04-22 01:44:14 +0300

    feat(app): Extend cards-table-columns.tsx config

--- app/src/app/dashboard/cards/_components/cards-table-columns.tsx
+++ app/src/app/dashboard/cards/_components/cards-table-columns.tsx
@@ -1,24 +1,70 @@
 "use client";
 
+import { CardStatusBadge } from "@/components/cards/access/card-status-badge";
+import { CardActions } from "@/components/cards/card-actions";
+import { DataTableColumnHeader } from "@/components/data-table/data-table-column-header";
+import { FormattedDate } from "@/components/data-table/formatted-date";
 import { type CardResponse } from "@/lib/validations/card";
 import { type ColumnDef } from "@tanstack/react-table";
+
+const translationKey = "Card.filter";
 
 export function getColumns(): ColumnDef<CardResponse>[] {
   return [
     {
       accessorKey: "holder",
-      enableSorting: false,
       enableHiding: false,
+      header: ({ column }) => (
+        <DataTableColumnHeader
+          column={column}
+          title={`${translationKey}.holder.label`}
+        />
+      ),
+      cell: ({ row }) => (
+        <span className="xl:min-w-64">{row.getValue("holder")}</span>
+      ),
+      meta: {
+        headerName: `${translationKey}.holder.label`,
+      },
     },
     {
       accessorKey: "active",
       enableSorting: false,
       enableHiding: false,
+      header: ({ column }) => (
+        <DataTableColumnHeader
+          column={column}
+          title={`${translationKey}.active.label`}
+        />
+      ),
+      cell: ({ row }) => <CardStatusBadge active={row.original.active} />,
+      meta: {
+        headerName: `${translationKey}.active.label`,
+      },
     },
     {
       accessorKey: "createdAt",
-      enableSorting: false,
       enableHiding: false,
+      header: ({ column }) => (
+        <DataTableColumnHeader
+          column={column}
+          title={`${translationKey}.created_at.label`}
+        />
+      ),
+      cell: ({ row }) => (
+        <FormattedDate
+          date={new Date(Number(row.getValue("createdAt")) * 1000)}
+        />
+      ),
+      meta: {
+        headerName: `${translationKey}.created_at.label`,
+      },
+    },
+    {
+      id: "actions",
+      cell: ({ row }) => (
+        <CardActions key={row.original.id} card={row.original} />
+      ),
     },
   ];
 }
