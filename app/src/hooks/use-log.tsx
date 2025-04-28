9bd1932b1aaa3e0076af421c70229b5ee95f6544
Hristiyan Dimitrov <90335627+Crea7orX@users.noreply.github.com>
2025-04-28 09:01:38 +0000

    refactor(app): Simplify use-log.tsx hook by utilizing getLog for log data processing

--- app/src/hooks/use-log.tsx
+++ app/src/hooks/use-log.tsx
@@ -1,36 +1,17 @@
-import { LogDisplayInfos } from "@/config/logs";
+import { getLog } from "@/lib/logs";
 import type { LogResponse } from "@/lib/validations/log";
-import { CircleHelp } from "lucide-react";
 import { useTranslations } from "next-intl";
 import * as React from "react";
 
 export function useLog(log: LogResponse) {
   const t = useTranslations();
 
-  return React.useMemo(() => {
-    const logDisplayInfo = LogDisplayInfos[log.action] ?? {
-      title: "Log.logs.unknown.title",
-      text: ({ t }) => t("Log.logs.unknown.text"),
-      actor: ({ t }) => t("Log.activity_card.by.unknown"),
-      icon: CircleHelp,
-      color: "warning",
-    };
-
-    const actionActor =
-      log.actorId === "system"
-        ? t("Log.card.actor.system")
-        : `${log.actorName ?? ""}${log.actorEmail ? (log.actorName ? ` (${log.actorEmail})` : `${log.actorEmail}`) : ""}`;
-    const actionShortActor =
-      log.actorId === "system"
-        ? t("Log.activity_card.by.system")
-        : `${log.actorName ?? log.actorEmail ?? t("Log.activity_card.by.unknown")}`;
-
-    return {
-      ...logDisplayInfo,
-      text: logDisplayInfo.text({ t, log, actionActor }),
-      actor: logDisplayInfo.actor
-        ? logDisplayInfo.actor({ t, log, actionActor: actionShortActor })
-        : actionShortActor,
-    };
-  }, [log, t]);
+  return React.useMemo(
+    () =>
+      getLog({
+        t,
+        logData: log,
+      }),
+    [log, t],
+  );
 }
