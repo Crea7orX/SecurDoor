5eb1be65e84fc537bb802adb2515181908b5a3d4
Hristiyan Dimitrov <90335627+Crea7orX@users.noreply.github.com>
2025-04-28 21:16:14 +0000

    feat(app): Create webhooks db schema.ts

---
+++ app/src/server/db/webhooks/schema.ts
@@ -0,0 +1,21 @@
+import IdPrefix, { generateId } from "@/lib/ids";
+import { sql } from "drizzle-orm";
+import { integer, pgEnum, pgTable, text, varchar } from "drizzle-orm/pg-core";
+
+export const webhookTypeEnum = pgEnum("webhook_type", ["discord", "slack"]);
+
+export const webhooks = pgTable("webhooks", {
+  id: varchar("id", { length: 256 })
+    .primaryKey()
+    .$default(() => generateId(IdPrefix.WEBHOOK)),
+  name: varchar("name", { length: 256 }).notNull(),
+  type: webhookTypeEnum("type").notNull(),
+  url: text("url").notNull(),
+  ownerId: varchar("owner_id", { length: 256 }).notNull(),
+  createdAt: integer("created_at")
+    .default(sql`(EXTRACT(EPOCH FROM NOW()))`)
+    .notNull(),
+  updatedAt: integer("updated_at").$onUpdate(
+    () => sql`(EXTRACT(EPOCH FROM NOW()))`,
+  ),
+});
