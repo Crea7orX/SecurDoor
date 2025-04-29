d3a222403920652849c114cfe00c68626ed8a94b
Hristiyan Dimitrov <90335627+Crea7orX@users.noreply.github.com>
2025-04-29 16:10:11 +0000

    feat(app): Create webhook-test-alert-dialog.tsx component

---
+++ app/src/components/webhooks/webhook-test-alert-dialog.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { useTestWebhookMutation } from "@/hooks/api/webhooks/use-test-webhook-mutation";
+import { useTranslations } from "next-intl";
+import * as React from "react";
+import { toast } from "sonner";
+
+interface WebhookTestAlertDialogProps
+  extends React.ComponentPropsWithoutRef<typeof AlertDialog> {
+  id: string;
+}
+
+export function WebhookTestAlertDialog({
+  id,
+  children,
+  ...props
+}: WebhookTestAlertDialogProps) {
+  const t = useTranslations("Webhook.test.alert");
+  const tButton = useTranslations("Common.button");
+
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const { mutateAsync: test } = useTestWebhookMutation({
+    id: id,
+  });
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const handleTest = async () => {
+    setIsLoading(true);
+    const toastId = toast.loading(t("notification.loading"));
+    await test()
+      .then(() => {
+        toast.warning(t("notification.success"), {
+          id: toastId,
+        });
+
+        setIsOpen(false);
+      })
+      .catch(() => {
+        toast.error(t("notification.error"), {
+          id: toastId,
+        });
+      });
+
+    setIsLoading(false);
+  };
+
+  return (
+    <AlertDialog {...props} open={isOpen} onOpenChange={setIsOpen}>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{t("title")}</AlertDialogTitle>
+          <AlertDialogDescription>{t("description")}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel disabled={isLoading}>
+            {tButton("cancel")}
+          </AlertDialogCancel>
+          <Button disabled={isLoading} onClick={() => handleTest()}>
+            {tButton("continue")}
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
