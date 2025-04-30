f369dc58b8d780f0daca9284b68df7592b357cfe
Hristiyan Dimitrov <90335627+Crea7orX@users.noreply.github.com>
2025-04-30 00:19:26 +0000

    feat(app): Create /api/webhooks/[id] PUT route.ts

--- app/src/app/api/webhooks/[id]/route.ts
+++ app/src/app/api/webhooks/[id]/route.ts
@@ -1,12 +1,30 @@
 import { authenticate } from "@/lib/auth";
 import { handleError } from "@/lib/errors";
 import { NotFoundError } from "@/lib/exceptions";
-import { webhookResponseSchema } from "@/lib/validations/webhook";
-import { webhookDelete } from "@/server/db/webhooks/queries";
+import { webhookResponseSchema, type WebhookUpdate, webhookUpdateSchema } from "@/lib/validations/webhook";
+import { webhookDelete, webhookUpdate } from "@/server/db/webhooks/queries";
 import { type NextRequest, NextResponse } from "next/server";
 
 interface WebhookByIdProps {
   params: Promise<{ id: string }>;
+}
+
+// update webhook
+export async function PUT(request: NextRequest, props: WebhookByIdProps) {
+  try {
+    const { id } = await props.params;
+    const { userId, ownerId } = await authenticate(request);
+
+    const json = (await request.json()) as WebhookUpdate;
+    const update = webhookUpdateSchema.parse(json);
+
+    const webhook = await webhookUpdate({ id, update, userId, ownerId });
+    if (!webhook) throw new NotFoundError();
+
+    return NextResponse.json(webhookResponseSchema.parse(webhook));
+  } catch (error) {
+    return handleError(error);
+  }
 }
 
 // delete webhook
