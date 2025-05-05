8d3ce9d15ceabffee6d43aaca8afb557d32a8d55
Hristiyan Dimitrov <90335627+Crea7orX@users.noreply.github.com>
2025-05-05 22:17:43 +0000

    feat(app): Add biometric lock and unlock log types in logs.tsx

--- app/src/config/logs.tsx
+++ app/src/config/logs.tsx
@@ -509,6 +509,52 @@ export const LogDisplayInfos: Record<string, LogDisplayInfo> = {
     color: "warning",
   },
 
+  // Reference: [biometricId, individual, deviceSerialId, deviceName]
+  "biometric.lock": {
+    title: "Log.logs.biometric.lock.title",
+    text: ({ t, log }) =>
+      t.rich("Log.logs.biometric.lock.text", {
+        actor: (log.reference?.[1] as string) ?? "",
+        biometricId: (log.reference?.[0] as string) ?? "",
+        serialId: (log.reference?.[2] as string) ?? "",
+        deviceName: (log.reference?.[3] as string) ?? "",
+      }),
+    actor: ({ t, log }) => {
+      const actor = log.reference?.[1] as string;
+
+      if (actor === "NULL") {
+        return t("Log.activity_card.by.unknown");
+      }
+
+      return actor ?? t("Log.activity_card.by.unknown");
+    },
+    icon: Lock,
+    color: "destructive",
+  },
+
+  // Reference: [biometricId, individual, deviceSerialId, deviceName]
+  "biometric.unlock": {
+    title: "Log.logs.biometric.unlock.title",
+    text: ({ t, log }) =>
+      t.rich("Log.logs.biometric.unlock.text", {
+        actor: (log.reference?.[1] as string) ?? "",
+        biometricId: (log.reference?.[0] as string) ?? "",
+        serialId: (log.reference?.[2] as string) ?? "",
+        deviceName: (log.reference?.[3] as string) ?? "",
+      }),
+    actor: ({ t, log }) => {
+      const actor = log.reference?.[1] as string;
+
+      if (actor === "NULL") {
+        return t("Log.activity_card.by.unknown");
+      }
+
+      return actor ?? t("Log.activity_card.by.unknown");
+    },
+    icon: LockOpen,
+    color: "success",
+  },
+
   // Reference: [name]
   "tag.create": {
     title: "Log.logs.tag.create.title",
